db99c9dca3e49702c2c01cdcdd8255992eb7d785
KrzysztofSym <72348036+KrzysztofSym@users.noreply.github.com>
2026-06-08 22:08:58 +0200

    Square class finished

--- Polygon_area_calc.py
+++ Polygon_area_calc.py
@@ -20,26 +20,32 @@ class Rectangle:
     def get_diagonal(self):
         return((self.width **2 + self.height **2) **0.5)
     def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return 'Too big for picture.'
         picture = ""
         for _ in range(self.height):
             row = ("*" * self.width) + "\n"
             picture += row
-            return picture
-    def get_amount_inside(self):
-        pass
+        return picture
+    def get_amount_inside(self, shape):
+        fit_width = self.width // shape.width
+        fit_height = self.height // shape.height
+        return fit_width * fit_height
 
 class Square(Rectangle):
     def __init__(self, side):
         super().__init__(side, side)
     def set_width(self, side):
         if side <= 0:
-            raise ValueError("Side has to be positive")
-        self.side = side  
-    def set_height(self, side):
-        if side <= 0:
             raise ValueError("Length has to be positive")
-        self.side = side
-        
+        self.width = side
+        self.height = side
+    def set_height(self, side):
+        self.set_width(side)
+    def set_side(self, side):
+        self.set_width(side)    
+    def __str__(self):
+        return(f'Square(side={self.width})')  
         
 rect = Rectangle(10, 5)
 print(rect.get_area())
@@ -47,3 +53,7 @@ print(rect.get_perimeter())
 print(rect.get_diagonal())
 print(rect.get_picture())
 print(rect)
+
+sq = Square(9)
+print(sq.get_picture())
+print(sq)
